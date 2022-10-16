ae8f6faf15565be7d0a3bc92791f7a5c36e2a0b8
Khalid Atayee <khalidatayee.76@gmail.com>
2022-10-16 13:01:15 +0430

    user authenticaiton is about finish

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -4,17 +4,39 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     function register(Request $request){
 
-        $users = new User();
-        $users->name=$request->username;
-        $users->email=$request->email;
-        $users->password=md5($request->password);
-        $users->save();
-        return response()->json(['message'=>'You have succssfuly signed up']);
+            $user = new User();
+            $user->name=$request->username;
+            $user->email=$request->email;
+            $user->password=Hash::make($request->password);
+            $user->save();
+
+            return response()->json([
+                'status'=>true,
+                'message'=>'user created successfully',
+
+
+            ],200);
+
+
+
+    }
+    function login(Request $request){
+        $user = User::where('email',$request->email)->first();
+        if($user && Hash::check($request->password,$user->password)){
+            Auth::login($user);
+            return response()->json([
+               'status'=>true,
+               'Token'=>$user->createToken('API Token')->plainTextToken,
+            ]);
+
+        }
 
     }
 }
